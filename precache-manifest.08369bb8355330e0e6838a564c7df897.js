2d2de6ceb164f3493cc740752feba395b3e8abd1
Nicholas-Lin <nicholaslin10@gmail.com>
2023-12-10 13:42:46 -0800

    Updates

---
+++ precache-manifest.08369bb8355330e0e6838a564c7df897.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714e035e6578351160d78ee5b8ba677f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96f60d80cd71ae009324",
+    "url": "/static/css/2.3003b959.chunk.css"
+  },
+  {
+    "revision": "6d8b0460845850892b36",
+    "url": "/static/css/main.dc1c42f3.chunk.css"
+  },
+  {
+    "revision": "96f60d80cd71ae009324",
+    "url": "/static/js/2.74ff7e61.chunk.js"
+  },
+  {
+    "revision": "4ed2d4f00cf8324f872f01907affe619",
+    "url": "/static/js/2.74ff7e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8b0460845850892b36",
+    "url": "/static/js/main.aa1b0027.chunk.js"
+  },
+  {
+    "revision": "2c28acb385e91a8e5ebe",
+    "url": "/static/js/runtime-main.752ac9d2.js"
+  },
+  {
+    "revision": "f720df37ff3964df6e4b92146a1fe97e",
+    "url": "/static/media/cassette.f720df37.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vibe/precache-manifest.4e1a664789f4a8f33c097ee4eba68362.js"
+  "/precache-manifest.08369bb8355330e0e6838a564c7df897.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vibe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
